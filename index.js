4218110db9c950b8533754bb0f7214bdc6c943fe
rasheeda <raskieda@gmail.com>
2024-03-28 13:25:23 +0200

    changed div to h3

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-// Sample menu data (Consider fetching this data from a server in a real-world scenario)
+// Sample menu data
 const menu = {
     Starters: [
-        {name: "Garlic Bread", price: 100},
-        {name: "Bruschetta", price: 150}
+        {name: "Garlic Bread", price: 60},
+        {name: "Bruschetta", price: 80}
     ],    
     MainCourses: [
-        {name: "Margherita Pizza", price: 80}, 
-        {name: "Spaghetti Carbonara", price: 95}
+        {name: "Margherita Pizza", price: 100}, 
+        {name: "Spaghetti Carbonara", price: 130}
     ],
     Desserts: [
-        {name: "Tiramisu", price: 120},
-        {name: "Cheesecake", price: 65}
+        {name: "Tiramisu", price: 110},
+        {name: "Cheesecake", price: 60}
     ]
 };
 
 // Function to display menu items by category
 function displayMenuItems(menu) {
     // Get the menu container element from the HTML
-    const menuDiv = document.getElementById('menu');
+    const menuElement = document.getElementById('menu');
     // Loop through each category and its items in the menu object
     for (const [category, items] of Object.entries(menu)) {
         // Create an element to represent the category
-        const categoryElement = document.createElement('div');
+        const categoryElement = document.createElement('h3');
         // Set the text content of the category element to the category name
         categoryElement.textContent = category;
         // Append the category element to the menu container
-        menuDiv.appendChild(categoryElement);
+        menuElement.appendChild(categoryElement);
 
         // Create an element to represent a list of items
         const itemListElement = document.createElement('ul');
         // Append a list of items element to the menu container
-        menuDiv.appendChild(itemListElement);
+        menuElement.appendChild(itemListElement);
 
         // Loop through the items in the category and create list items
         for (let i = 0; i < items.length; i++) {
@@ -38,7 +38,7 @@ function displayMenuItems(menu) {
             // Create a list item element
             const listItemElement = document.createElement('li');
             // Set the text content of the list item element to the item name and price
-            listItemElement.textContent = `${item.name} - R${item.price}`;
+            listItemElement.textContent = `${item.name}`;
             // Attach a click event listener to the list item to add it to the order
             listItemElement.addEventListener('click', (event) => {
                 console.log(`${item.name} added to the order...`);
@@ -50,7 +50,7 @@ function displayMenuItems(menu) {
     }
 }
 
-// Variable to store the total order price
+// Variable that stores the total order price:
 let total = 0;
 
 // Callback function for adding an item to the order
